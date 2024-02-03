13cd84246c9c82c19fc4c144c8a4b0ab3e995595
Sniphs98 <Lukas@bronstering.com>
2024-02-04 00:24:49 +0100

    version finder working

--- main.py
+++ main.py
@@ -12,10 +12,34 @@ driver.get(URL)
 driver.implicitly_wait(5)
 time.sleep(3)
 
-elements = driver.find_elements(By.CLASS_NAME, "c-entry-box--compact__title")
+game_name = 'Genshin Impact version'
+
+def contains_numbers(text):
+    for char in text:
+        if char.isdigit():
+            return True
+    return False
+
+def find_highest_version(search_results):
+    version_string = "0"
+    newest_element = None
+    for element in search_results:
+        if (game_name in element.text):
+            ##print(element.text.split("version ")[1].split()[0])
+            if float(element.text.split("version ")[1].split()[0]) > float(version_string):
+                print(element.text.split("version ")[1].split()[0])
+                version_string = element.text.split("version ")[1].split()[0]
+                newest_element = element
+    return newest_element
+
+search_results = driver.find_elements(By.CLASS_NAME, "c-entry-box--compact__title")
 with open('scraper.html', 'w') as file:
-    for element in elements:
+    element = find_highest_version(search_results)
+    print(element.text)
+    for element in search_results:
         file.write(element.get_attribute("innerHTML"))
         file.write("\n")
 
 print("finished.")
+
+
