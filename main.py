d9917b1ad497831b8cc637a7be85ddad8ec9ea28
lbronste <lukas.bronstering@hs-osnabrueck.de>
2024-02-07 17:41:31 +0100

    fixed ntfy version detection

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import re
 
 URL = "https://game8.co/games/Genshin-Impact/search?q=Redeem+Codes"
 last_version_string = ""
+version_string = "0.0"
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get(URL)
@@ -31,9 +32,9 @@ def filter_redeem_codes(search_results):
     return redeem_codes_list
 
 def find_highest_version(search_results):
+    global version_string
     pattern = "(\d+\.\d+)"
     newest_element = None
-    version_string = "0.0"
     for element in search_results:
         match = re.match(pattern, element.text, re.IGNORECASE)
         if match:
@@ -42,6 +43,10 @@ def find_highest_version(search_results):
                 newest_element = element
     return newest_element
 
+def write_newest_version_to_file():
+    with open('last_version.txt', 'w') as file:
+        file.write(highest_version_element.text)
+        
 search_results = driver.find_elements(By.CLASS_NAME, "c-archiveSearchListItem")
 search_results = filter_redeem_codes(search_results)
 highest_version_element = find_highest_version(search_results)
@@ -49,12 +54,19 @@ highest_version_element = find_highest_version(search_results)
 with open('last_version.txt', 'r') as file:
     last_version_string = file.read()
 
-with open('last_version.txt', 'w') as file:
-    if(highest_version_element.text not in last_version_string):
+if(last_version_string):
+    print("in last string")
+    if(last_version_string.find(version_string)):
         print("-New Version")
-        #requests.post("https://ntfy.sh/genshin_codes",
-        #data="New Version is Up 🚀🎉".encode(encoding='utf-8'))
-        file.write(highest_version_element.text)
+        requests.post("https://ntfy.sh/genshin_codes",
+        data="New Version is Up 🚀🎉".encode(encoding='utf-8'))
+        write_newest_version_to_file()
+else:
+        print("-Empty file")
+        requests.post("https://ntfy.sh/genshin_codes",
+        data="New Version is Up 🚀🎉".encode(encoding='utf-8'))
+        write_newest_version_to_file()
+    
 
 #temp = highest_version_element.find_element(By.XPATH, '//a[contains(@href, "440922")]')
 element = highest_version_element.find_element(By.CLASS_NAME,"c-archiveSearchListItem__link")
@@ -66,12 +78,13 @@ tables = driver.find_elements(By.CLASS_NAME, "a-table")
 #for table in tables:
     #print("-----------------------------------------------------------------")
     #print(table.get_attribute("innerHTML"))
-
+code = []
 tr_elements = tables[0].find_elements(By.TAG_NAME,"tr")
 for tr in tr_elements:
     center_elements = tr.find_elements(By.CLASS_NAME,"center")
     for element in center_elements:
-        print(element.text)
+        #print(element.text)
+        code.append(element.text)
 
 print("finished.")
 
