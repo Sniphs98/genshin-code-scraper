dda4db76865525d2488b4ddbebc9e7ad64f1ac84
lbronste <lukas.bronstering@hs-osnabrueck.de>
2024-02-07 15:56:20 +0100

    geting the code. need to fix ntfy

--- main.py
+++ main.py
@@ -49,31 +49,29 @@ highest_version_element = find_highest_version(search_results)
 with open('last_version.txt', 'r') as file:
     last_version_string = file.read()
 
-print(last_version_string)
-
 with open('last_version.txt', 'w') as file:
     if(highest_version_element.text not in last_version_string):
         print("-New Version")
-        requests.post("https://ntfy.sh/genshin_codes",
-        data="New Version is Up 🚀🎉".encode(encoding='utf-8'))
+        #requests.post("https://ntfy.sh/genshin_codes",
+        #data="New Version is Up 🚀🎉".encode(encoding='utf-8'))
         file.write(highest_version_element.text)
-    
 
-#print(highest_version_element.get_attribute("innerHTML"))
-#print(highest_version_element.text)
-
-driver.get("https://game8.co/games/Genshin-Impact/archives/440922")
+#temp = highest_version_element.find_element(By.XPATH, '//a[contains(@href, "440922")]')
+element = highest_version_element.find_element(By.CLASS_NAME,"c-archiveSearchListItem__link")
+element.click()
 time.sleep(2)
 
+
 tables = driver.find_elements(By.CLASS_NAME, "a-table")
-
 #for table in tables:
-#    print(table)
+    #print("-----------------------------------------------------------------")
+    #print(table.get_attribute("innerHTML"))
 
-#print(highest_version_element.get_attribute("innerHTML"))
-
-#with open('scraper.html', 'a') as file:
-#    file.write()
+tr_elements = tables[0].find_elements(By.TAG_NAME,"tr")
+for tr in tr_elements:
+    center_elements = tr.find_elements(By.CLASS_NAME,"center")
+    for element in center_elements:
+        print(element.text)
 
 print("finished.")
 
